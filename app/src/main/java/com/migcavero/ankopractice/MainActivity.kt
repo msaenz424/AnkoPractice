dc729b490e70e5b8f3754f9723718dd9b92f7b65
Miguel Saenz <msaenz424@gmail.com>
2017-12-11 20:12:01 -0800

    Anko Layouts: replaced xml with anko

--- app/src/main/java/com/migcavero/ankopractice/MainActivity.kt
+++ app/src/main/java/com/migcavero/ankopractice/MainActivity.kt
@@ -2,19 +2,30 @@ package com.migcavero.ankopractice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.sendSMS
+import android.view.Gravity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        text_button.setOnClickListener{
-            var phoneNumber: String = phone_text_view.text.toString()
-            phoneNumber = "+1${phoneNumber.substring(1..3)}${phoneNumber.substring(5..7)}${phoneNumber.substring(9..12)}"
-            sendSMS("$phoneNumber", "Hello +1${phone_text_view.text}!!!")
+        verticalLayout{
+
+            val phoneNumberTextView = textView("(123)456-7890"){
+                textSize = 24f
+                gravity = Gravity.CENTER_HORIZONTAL
+            }
+
+            button("Send SMS") {
+                onClick {
+                    var phoneNumber: String = phoneNumberTextView.text.toString()
+                    phoneNumber = "+1${phoneNumber.substring(1..3)}${phoneNumber.substring(5..7)}${phoneNumber.substring(9..12)}"
+                    sendSMS("$phoneNumber", "Hello ${phoneNumberTextView.text}!!!")
+                }
+            }
+
         }
     }
 }
